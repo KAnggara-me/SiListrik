87bcfc20d57e36ab8709a0e6c329691b188e5dd4
Call Vin <kanggara75@gmail.com>
2022-08-31 12:24:03 +0700

    fix: ignote scantum on migration

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,27 +2,29 @@
 
 namespace App\Providers;
 
+use Laravel\Sanctum\Sanctum;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    Sanctum::ignoreMigrations();
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    //
+  }
 }
