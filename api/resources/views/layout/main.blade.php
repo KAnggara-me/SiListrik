73c974bc472cb824719c1c27c522240142038540
Call Vin <kanggara75@gmail.com>
2022-10-17 14:52:18 +0700

    fix: resource layout

--- api/resources/views/layout/main.blade.php
+++ api/resources/views/layout/main.blade.php
@@ -9,10 +9,10 @@
   <meta name="keywords" content="api,wa,listrik">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
   <title>{{ $title }} | SiListrik</title>
 
-  @if ($title == 'home')
+  @if ($title == 'Home')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
   @endif
 
@@ -33,74 +33,41 @@
   <script src="js/app.js"></script>
 
   <script>
-    function getStatus() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function() {}
-    }
-    const status = {{ Js::from(auth()->user()->status) }};
-
-    if (status == 1)
-      console.log('user aktif');
-    else
-      console.log('user tidak aktif');
-  </script>
-
-  {{-- <script>
-    var qrCode = document.getElementById("qrCode");
-    var qrCodeValue = "";
-
-    var token = {{ Js::from(auth()->user()->token) }};
-    var id = {{ Js::from(auth()->user()->username) }};
-    var status = {{ Js::from(auth()->user()->status) }};
+    const xhttp = new XMLHttpRequest();
+    const username = {{ Js::from(auth()->user()->username) }};
+    const token = {{ Js::from(auth()->user()->token) }};
+    const initStatus = {{ Js::from(auth()->user()->status) }};
+    const url = 'api/v1/status/' + username + '/' + token;
+    let status = initStatus;
 
     setInterval(function() {
       // Create a ajax Object
       var xhr = new XMLHttpRequest();
-      xhr.open("GET", "/status", true);
+      xhr.open("GET", url, true);
       xhr.setRequestHeader("Accept", "application/json");
-      xhr.setRequestHeader("Authorization", "Bearer " + id);
+      xhr.setRequestHeader("Authorization", "Bearer " + token);
 
       // CHeck if the ajax object is ready
       xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var data = JSON.parse(this.responseText);
           status = data.status;
-          console.log(status)
-          console.log(typeof status)
         }
       };
       xhr.send();
 
-      if (status == 0) {
-        console.log("Not OK");
-        getqr();
-      } else {
-        console.log("OK");
-        // window.location.href = "home";
+      if (status != initStatus) {
+        console.log('status berubah')
+        if (status == 0) {
+          console.log("User disconnected");
+          window.location.href = "connect";
+        } else {
+          console.log("User connected");
+          window.location.href = "home";
+        }
       }
-
     }, 2000);
-
-    function getqr(params) {
-      console.log("Running");
-      // // Create a a jax Object
-      // var xhr1 = new XMLHttpRequest();
-
-      // xhr1.open("GET", "/qr", true);
-      // xhr1.setRequestHeader("Accept", "application/json");
-      // xhr1.setRequestHeader("Authorization", "Bearer " + token);
-
-      // // CHeck if the ajax object is ready
-      // xhr1.onreadystatechange = function() {
-      //   if (this.readyState == 4 && this.status == 200) {
-      //     qrCode.innerHTML = xhr1.responseText;
-      //   }
-      // };
-      // console.log(xhr1.responseText);
-      // xhr1.send();
-    }
-  </script> --}}
-
+  </script>
 </body>
 
 </html>
--- api/resources/views/layout/navbar.blade.php
+++ api/resources/views/layout/navbar.blade.php
@@ -3,15 +3,15 @@
     <div class="content-center justify-between text-left md:fixed md:left-0 md:top-0 md:mt-12 md:w-48 md:content-start">
       <ul class="list-reset flex flex-row px-1 pt-3 text-center md:flex-col md:py-3 md:px-2 md:text-left">
         <li class="mr-3 flex-1">
-          <a href="home" class="{{ $active === 'home' ? 'border-blue-600' : 'border-gray-800' }} block border-b-2 py-1 pl-1 align-middle text-white no-underline hover:text-white md:py-3">
-            <i class="fas fa-chart-area {{ $active === 'home' ? 'text-blue-600' : '' }} pr-0 md:pr-3">
-            </i><span class="block pb-1 text-xs text-white md:inline-block md:pb-0 md:text-base md:text-white">Analytics</span>
+          <a href="home" class="{{ $active === 'home' ? 'border-blue-600' : 'border-gray-800' }} block border-b-2 py-1 pl-1 align-middle text-white no-underline hover:border-blue-500 hover:text-white md:py-3">
+            <i class="fas fa-chart-area {{ $active === 'home' ? 'text-blue-600' : '' }} pr-0 md:pr-3"></i>
+            <span class="block pb-1 text-xs text-gray-400 md:inline-block md:pb-0 md:text-base md:text-gray-200">Dashboard</span>
           </a>
         </li>
 
         <li class="mr-3 flex-1">
           <a href="notif" class="{{ $active === 'notif' ? 'border-pink-600' : 'border-gray-800' }} block border-b-2 py-1 pl-1 align-middle text-white no-underline hover:border-pink-500 hover:text-white md:py-3">
-            <i class="fas fa-bell {{ $active === 'hotif' ? 'text-pink-600' : '' }} pr-0 md:pr-3"></i>
+            <i class="fas fa-bell {{ $active === 'notif' ? 'text-pink-600' : '' }} pr-0 md:pr-3"></i>
             <span class="block pb-1 text-xs text-gray-400 md:inline-block md:pb-0 md:text-base md:text-gray-200">Notification</span>
           </a>
         </li>
