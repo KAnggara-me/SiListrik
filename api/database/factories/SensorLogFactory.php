7f60995a0f38ad4d92e22d2d4189cc042401af3a
Call Vin <kanggara75@gmail.com>
2022-10-17 14:51:12 +0700

    add: sensor log factory

---
+++ api/database/factories/SensorLogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SensorLog>
+ */
+class SensorLogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'voltase' => $this->faker->randomFloat(2, 200, 230),
+            'arus' => $this->faker->randomFloat(2, 0, 10),
+            'temperatur' => $this->faker->randomFloat(2, 25, 120),
+            'kelembaban' => $this->faker->randomFloat(2, 10, 90),
+            'asap' => $this->faker->randomFloat(2, 200, 500),
+            'api' => $this->faker->randomElement([0, 1, 2, 3, 4, 5]),
+        ];
+    }
+}
