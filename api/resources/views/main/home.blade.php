615a9dc18e4f91895215da2e7ab92faeac6613fc
Call Vin <kanggara75@gmail.com>
2022-10-17 14:52:47 +0700

    fix: home views

--- api/resources/views/main/home.blade.php
+++ api/resources/views/main/home.blade.php
@@ -9,188 +9,190 @@
       </div>
       <div class="flex flex-wrap">
         <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Metric Card-->
           <div class="rounded-lg border-b-4 border-blue-500 bg-gradient-to-b from-blue-200 to-blue-100 p-5 shadow-xl">
             <div class="flex flex-row items-center">
               <div class="flex-shrink pr-4">
-                <div class="rounded-full bg-blue-600 p-5"><i class="fas fa-server fa-2x fa-inverse"></i></div>
+                <div class="rounded-full bg-blue-600 p-5">
+
+                  <i class="fas fa-bolt fa-2x fa-inverse"></i>
+                </div>
               </div>
               <div class="flex-1 text-right md:text-center">
-                <h2 class="font-bold uppercase text-gray-600">Server Uptime</h2>
-                <p class="text-3xl font-bold">152 days</p>
+                <h2 class="font-bold uppercase text-gray-600">Voltase</h2>
+                <p class="text-3xl font-bold">
+                  {{ $last->voltase }} Volt
+                </p>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
+        </div>
+        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
+          <div class="rounded-lg border-b-4 border-blue-500 bg-gradient-to-b from-blue-200 to-blue-100 p-5 shadow-xl">
+            <div class="flex flex-row items-center">
+              <div class="flex-shrink pr-4">
+                <div class="rounded-full bg-blue-600 p-5">
+                  <i class="fas fa-lightbulb fa-2x fa-inverse"></i>
+                </div>
+              </div>
+              <div class="flex-1 text-right md:text-center">
+                <h2 class="font-bold uppercase text-gray-600">Arus</h2>
+                <p class="text-3xl font-bold">
+                  {{ $last->arus }} A
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Metric Card-->
-          <div class="rounded-lg border-b-4 border-indigo-500 bg-gradient-to-b from-indigo-200 to-indigo-100 p-5 shadow-xl">
+          <div class="rounded-lg border-b-4 border-blue-500 bg-gradient-to-b from-blue-200 to-blue-100 p-5 shadow-xl">
             <div class="flex flex-row items-center">
               <div class="flex-shrink pr-4">
-                <div class="rounded-full bg-indigo-600 p-5"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
+                <div class="rounded-full bg-blue-600 p-5">
+
+                  <i class="fas fa-lightbulb fa-2x fa-inverse"></i>
+                </div>
               </div>
               <div class="flex-1 text-right md:text-center">
-                <h2 class="font-bold uppercase text-gray-600">To Do List</h2>
-                <p class="text-3xl font-bold">7 tasks</p>
+                <h2 class="font-bold uppercase text-gray-600">Daya</h2>
+                <p class="text-2xl font-bold">
+                  @php
+                    $V = $last->voltase;
+                    $I = $last->arus;
+                    $P = $V * $I;
+                    echo number_format($P, '1', '.', ',') . ' VA';
+                  @endphp
+                </p>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
         </div>
+
         <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Metric Card-->
-          <div class="rounded-lg border-b-4 border-red-500 bg-gradient-to-b from-red-200 to-red-100 p-5 shadow-xl">
+          <div class="{{ $last->temperatur > 30 ? 'border-red-500 from-red-200 to-red-100' : 'border-green-500 from-green-200 to-green-100' }} rounded-lg border-b-4 bg-gradient-to-b p-5 shadow-xl">
             <div class="flex flex-row items-center">
               <div class="flex-shrink pr-4">
-                <div class="rounded-full bg-red-600 p-5"><i class="fas fa-inbox fa-2x fa-inverse"></i></div>
+                <div class="{{ $last->temperatur > 30 ? 'bg-red-600' : 'bg-green-600' }} rounded-full p-5">
+                  <i class="fas {{ $last->temperatur > 30 ? 'fa-thermometer-full' : 'fa-thermometer-empty' }} fa-2x fa-inverse"></i>
+                </div>
               </div>
               <div class="flex-1 text-right md:text-center">
-                <h2 class="font-bold uppercase text-gray-600">Issues</h2>
-                <p class="text-3xl font-bold">3 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></p>
+                <h2 class="font-bold uppercase text-gray-600">Temperatur</h2>
+                <p class="text-2xl font-bold">
+                  <span class="font-normal italic"> {{ $last->temperatur }} C&deg; </span> |
+                  {{ $last->temperatur > 30 ? 'Suhu Tinggi' : 'Suhu Normal' }}
+                </p>
               </div>
             </div>
           </div>
-          <!--/Metric Card-->
+        </div>
+
+        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
+          <div class="{{ $last->asap > 300 ? 'border-red-500 from-red-200 to-red-100' : 'border-green-500 from-green-200 to-green-100' }} rounded-lg border-b-4 bg-gradient-to-b p-5 shadow-xl">
+            <div class="flex flex-row items-center">
+              <div class="flex-shrink pr-4">
+                <div class="{{ $last->asap > 300 ? 'bg-red-600' : 'bg-green-600' }} rounded-full p-5">
+                  <i class="fas fa-smoking fa-2x fa-inverse"></i>
+                </div>
+              </div>
+              <div class="flex-1 text-right md:text-center">
+                <h2 class="font-bold uppercase text-gray-600">Asap</h2>
+                <p class="text-2xl font-bold">
+                  {{ $last->asap > 300 ? 'Asap Terdeteksi' : 'Tidak Terdeteksi' }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
+          <div class="{{ $last->api > 0 ? 'border-red-500 from-red-200 to-red-100' : 'border-green-500 from-green-200 to-green-100' }} rounded-lg border-b-4 bg-gradient-to-b p-5 shadow-xl">
+            <div class="flex flex-row items-center">
+              <div class="flex-shrink pr-4">
+                <div class="{{ $last->api > 0 ? 'bg-red-600' : 'bg-green-600' }} rounded-full p-5">
+                  <i class="fas fa-fire-extinguisher fa-2x fa-inverse"></i>
+                </div>
+              </div>
+              <div class="flex-1 text-right md:text-center">
+                <h2 class="font-bold uppercase text-gray-600">Api</h2>
+                <p class="text-3xl font-bold">
+                  {{ $last->api > 0 ? 'Api Terdeteksi' : 'Tidak Terdeteksi' }}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
+      {{-- Log chart --}}
       <div class="mt-2 flex flex-grow flex-row flex-wrap">
-        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
+        <div class="w-full p-6 md:w-1/2 xl:w-1/2">
           <!--Graph Card-->
           <div class="rounded-lg border-transparent bg-white shadow-xl">
             <div class="rounded-tl-lg rounded-tr-lg border-b-2 border-gray-300 bg-gradient-to-b from-gray-300 to-gray-100 p-2 uppercase text-gray-800">
-              <h class="font-bold uppercase text-gray-600">Graph</h>
+              <h class="font-bold uppercase text-gray-600">Histori Daya (VA)</h>
             </div>
             <div class="p-5">
-              <canvas id="chartjs-7" class="chartjs" width="undefined" height="undefined"></canvas>
+              <canvas id="chartjs-daya" class="chartjs" width="undefined" height="undefined"></canvas>
               <script>
-                new Chart(document.getElementById("chartjs-7"), {
-                  "type": "bar",
+                let dateA = {!! json_encode($date, JSON_HEX_TAG) !!};
+                let daya = {!! json_encode($daya, JSON_HEX_TAG) !!};
+                new Chart(document.getElementById("chartjs-daya"), {
+                  "type": "line",
                   "data": {
-                    "labels": ["January", "February", "March", "April"],
-                    "datasets": [{
-                      "label": "Page Impressions",
-                      "data": [10, 20, 30, 40],
-                      "borderColor": "rgb(255, 99, 132)",
-                      "backgroundColor": "rgba(255, 99, 132, 0.2)"
-                    }, {
-                      "label": "Adsense Clicks",
-                      "data": [5, 15, 10, 30],
-                      "type": "line",
-                      "fill": false,
-                      "borderColor": "rgb(54, 162, 235)"
+                    labels: dateA,
+                    datasets: [{
+                      "label": "Daya (VA)",
+                      data: daya,
+                      borderColor: "rgba(255, 0, 132, 1)",
+                      fill: false
                     }]
                   },
                   "options": {
-                    "scales": {
-                      "yAxes": [{
-                        "ticks": {
-                          "beginAtZero": true
-                        }
-                      }]
-                    }
+                    legend: {
+                      display: false
+                    },
                   }
                 });
               </script>
             </div>
           </div>
-          <!--/Graph Card-->
         </div>
 
-        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Graph Card-->
+        <div class="w-full p-6 md:w-1/2 xl:w-1/2">
           <div class="rounded-lg border-transparent bg-white shadow-xl">
             <div class="rounded-tl-lg rounded-tr-lg border-b-2 border-gray-300 bg-gradient-to-b from-gray-300 to-gray-100 p-2 uppercase text-gray-800">
-              <h2 class="font-bold uppercase text-gray-600">Graph</h2>
+              <h2 class="font-bold uppercase text-gray-600">Histori Suhu (C&deg;)</h2>
             </div>
             <div class="p-5">
               <canvas id="chartjs-0" class="chartjs" width="undefined" height="undefined"></canvas>
               <script>
+                let suhu = {!! json_encode($suhu, JSON_HEX_TAG) !!};
+                let date = {!! json_encode($date, JSON_HEX_TAG) !!};
                 new Chart(document.getElementById("chartjs-0"), {
                   "type": "line",
                   "data": {
-                    "labels": ["January", "February", "March", "April", "May", "June", "July"],
+                    "labels": date,
                     "datasets": [{
-                      "label": "Views",
-                      "data": [65, 59, 80, 81, 56, 55, 40],
+                      "label": "Suhu (C)",
+                      "data": suhu,
                       "fill": false,
-                      "borderColor": "rgb(75, 192, 192)",
-                      "lineTension": 0.1
-                    }]
-                  },
-                  "options": {}
-                });
-              </script>
-            </div>
-          </div>
-          <!--/Graph Card-->
-        </div>
-
-        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Graph Card-->
-          <div class="rounded-lg border-transparent bg-white shadow-xl">
-            <div class="rounded-tl-lg rounded-tr-lg border-b-2 border-gray-300 bg-gradient-to-b from-gray-300 to-gray-100 p-2 uppercase text-gray-800">
-              <h2 class="font-bold uppercase text-gray-600">Graph</h2>
-            </div>
-            <div class="p-5">
-              <canvas id="chartjs-1" class="chartjs" width="undefined" height="undefined"></canvas>
-              <script>
-                new Chart(document.getElementById("chartjs-1"), {
-                  "type": "bar",
-                  "data": {
-                    "labels": ["January", "February", "March", "April", "May", "June", "July"],
-                    "datasets": [{
-                      "label": "Likes",
-                      "data": [65, 59, 80, 81, 56, 55, 40],
-                      "fill": false,
-                      "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)", "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"],
-                      "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"],
-                      "borderWidth": 1
+                      "borderColor": "rgb(75, 192, 50)",
+                      "lineTension": 0.5
                     }]
                   },
                   "options": {
-                    "scales": {
-                      "yAxes": [{
-                        "ticks": {
-                          "beginAtZero": true
-                        }
-                      }]
+                    legend: {
+                      display: false
                     }
                   }
                 });
               </script>
             </div>
           </div>
-          <!--/Graph Card-->
-        </div>
-
-        <div class="w-full p-6 md:w-1/2 xl:w-1/3">
-          <!--Graph Card-->
-          <div class="rounded-lg border-transparent bg-white shadow-xl">
-            <div class="rounded-tl-lg rounded-tr-lg border-b-2 border-gray-300 bg-gradient-to-b from-gray-300 to-gray-100 p-2 uppercase text-gray-800">
-              <h5 class="font-bold uppercase text-gray-600">Graph</h5>
-            </div>
-            <div class="p-5"><canvas id="chartjs-4" class="chartjs" width="undefined" height="undefined"></canvas>
-              <script>
-                new Chart(document.getElementById("chartjs-4"), {
-                  "type": "doughnut",
-                  "data": {
-                    "labels": ["P1", "P2", "P3"],
-                    "datasets": [{
-                      "label": "Issues",
-                      "data": [300, 50, 100],
-                      "backgroundColor": ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
-                    }]
-                  }
-                });
-              </script>
-            </div>
-          </div>
-          <!--/Graph Card-->
         </div>
       </div>
-    </div>
+
+
   </section>
 @endsection
