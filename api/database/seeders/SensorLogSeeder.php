d57e852f1697a9004670b698465b8617d59e6200
Call Vin <kanggara75@gmail.com>
2022-10-26 09:46:31 +0700

    add: varian sensor seeder

--- api/database/seeders/SensorLogSeeder.php
+++ api/database/seeders/SensorLogSeeder.php
@@ -15,5 +15,13 @@ class SensorLogSeeder extends Seeder
     public function run()
     {
         SensorLog::factory(250)->create();
+
+        SensorLog::create(['voltase' => 220, 'arus' => 4, 'temperatur' => 20, 'kelembaban' => 70, 'asap' => 250, 'api' => 0]); // Aman
+        SensorLog::create(['voltase' => 245, 'arus' => 1, 'temperatur' => 20, 'kelembaban' => 70, 'asap' => 250, 'api' => 0]); // Over Voltage
+        SensorLog::create(['voltase' => 220, 'arus' => 4, 'temperatur' => 70, 'kelembaban' => 70, 'asap' => 250, 'api' => 0]); // High Temp
+        SensorLog::create(['voltase' => 220, 'arus' => 9, 'temperatur' => 20, 'kelembaban' => 70, 'asap' => 250, 'api' => 0]); // Over Load
+        SensorLog::create(['voltase' => 220, 'arus' => 4, 'temperatur' => 70, 'kelembaban' => 70, 'asap' => 450, 'api' => 3]); // Kebakaran
+        SensorLog::create(['voltase' => 220, 'arus' => 4, 'temperatur' => 24, 'kelembaban' => 24, 'asap' => 450, 'api' => 0]); // Asap
+        SensorLog::create(['voltase' => 220, 'arus' => 4, 'temperatur' => 20, 'kelembaban' => 20, 'asap' => 250, 'api' => 3]); // Api
     }
 }
