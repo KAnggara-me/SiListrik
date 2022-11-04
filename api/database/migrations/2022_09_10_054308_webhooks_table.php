7e7cac8ab20e07aa1c19544229acfbe5ec7a4035
Call Vin <kanggara75@gmail.com>
2022-11-04 11:22:23 +0700

    fix: webhook status, msg, and type

--- api/database/migrations/2022_09_10_054308_webhooks_table.php
+++ api/database/migrations/2022_09_10_054308_webhooks_table.php
@@ -17,15 +17,15 @@ return new class extends Migration
 		Schema::create('webhooks', function (Blueprint $table) {
 			$table->string('id')->unique()->primary();
 			$table->enum('type', ['send_message_response', 'incoming_message', 'device_status_changed']); //send_message_response]]]]]]]]]]
-			$table->enum('status', ['success', 'fail']); // fail or success
+			$table->enum('status', ['success', 'fail', 'pending']); // fail or success
 			$table->string('webhook_msg'); // message
 
 			// Paylod section
-			$table->string('message'); // message
+			$table->string('message')->nullable(); // message
 			$table->string('phone_number'); // phone number
 			$table->string('device_id');
 			$table->foreign('device_id')->references('username')->on('users');
-			$table->string('message_type'); // message type
+			$table->string('message_type')->default('text'); // message type
 			$table->string('caption')->nullable();
 
 			$table->timestamps();
