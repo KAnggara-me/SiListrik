fd60834d49fc46bed65abb1279cc1c886733789e
Call Vin <kanggara75@gmail.com>
2022-11-05 18:52:54 +0700

    fix: diff incoming msg and response

--- api/resources/views/main/logs.blade.php
+++ api/resources/views/main/logs.blade.php
@@ -27,29 +27,33 @@
 						<tbody>
 							@foreach ($logs as $log)
 								<tr class="border-b border-gray-200 text-center text-xs text-black md:text-sm">
-									<td class="bg-grey-100 border-r border-gray-200 p-2">
+									<td
+										class="{{ $log->webhook_msg == 'incoming_message' ? 'bg-gray-100' : 'bg-gray-200' }} border-r border-gray-300 p-2">
 										{{ $log->updated_at->format('d/m/y H:i:s') }}
 									</td>
-
-									<td class="border-r border-gray-200 bg-gray-100 p-2">
+									<td
+										class="{{ $log->webhook_msg == 'incoming_message' ? 'bg-gray-100' : 'bg-gray-200' }} border-r border-gray-300 p-2">
 										<a href="http://wa.me/{{ $log->phone_number }}">
 											{{ substr($log->phone_number, 0, 5) . ' ' . substr($log->phone_number, 5, 4) . ' ' . substr($log->phone_number, 9, 4) . ' ' . substr($log->phone_number, 13, 2) }}
 										</a>
 									</td>
-									<td class="border-r border-gray-200 bg-gray-100 p-2">
+									<td
+										class="{{ $log->webhook_msg == 'incoming_message' ? 'bg-gray-100' : 'bg-gray-200' }} border-r border-gray-300 p-2">
 										{{ preg_replace('/[^A-Za-z0-9\-]/', ' ', $log->message) }}
 									</td>
-									<td class="border-r border-gray-200 bg-gray-100 p-2">
+									<td
+										class="{{ $log->webhook_msg == 'incoming_message' ? 'bg-gray-100' : 'bg-gray-200' }} border-r border-gray-300 p-2">
 										@if ($log->type == 'send_message_response')
 											Pesan Keluar
 										@elseif ($log->type == 'incoming_message')
 											Pesan Masuk
 										@endif
 									</td>
-									<td class="{{ $log->status == 'pending' ? 'bg-blue-300' : 'bg-green-400' }} border-r border-gray-200 p-2">
+									<td class="{{ $log->status == 'pending' ? 'bg-blue-300' : 'bg-green-400' }} border-r border-gray-300 p-2">
 										{{ ucfirst($log->status) }}
 									</td>
-									<td class="border-r border-gray-200 bg-gray-100 p-2">
+									<td
+										class="{{ $log->webhook_msg == 'incoming_message' ? 'bg-gray-100' : 'bg-gray-200' }} border-r border-gray-300 p-2">
 										@if ($log->webhook_msg == 'send_message_response')
 											Pesan Keluar
 										@elseif ($log->webhook_msg == 'incoming_message')
@@ -58,6 +62,7 @@
 											{{ $log->webhook_msg }}
 										@endif
 									</td>
+
 								</tr>
 							@endforeach
 						</tbody>
