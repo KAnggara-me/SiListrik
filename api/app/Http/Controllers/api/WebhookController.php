263a5cdd58978f4f5de5ae9225e38a52ab8c1384
Call Vin <kanggara75@gmail.com>
2022-11-06 21:42:00 +0700

    fix: rollback send image #api add api key

--- api/app/Http/Controllers/api/WebhookController.php
+++ api/app/Http/Controllers/api/WebhookController.php
@@ -87,11 +87,10 @@ class WebhookController extends Controller
 				$daya = ($data->arus * $data->voltase) == 0 ? "_error_" : number_format(($data->arus * $data->voltase), '0', ',', '.');
 				$caption = "\n_Status Sensor_\n\n*Daya Digunakan:* " . $daya . " VA" . "\n*Temperatur:* " . number_format(($data->temperatur), '0', ',', '.') . "C°" . "\n*Asap:* " . number_format(($data->asap), '0', ',', '.') . "ppm" . "\n\n```Update Terakhir:``` " . $date . "\n";
 				$token = User::where('username', $username)->first()->apitoken;
-				// $time = time();
-				// $image = getImage($time);
-				// $isExist = file_exists(public_path('images/' . $time . '.jpg'));
-				$msg = "https://silistrik.apiwa.tech/images/img.jpg";
-				// $msg = ($isExist && $image) ? "https://silistrik.apiwa.tech/images/" . $time . ".jpg" : "https://silistrik.apiwa.tech/images/img.jpg";
+				$time = time();
+				$image = getImage($time);
+				$isExist = file_exists(public_path('images/' . $time . '.jpg'));
+				$msg = ($isExist && $image) ? "https://silistrik.apiwa.tech/images/" . $time . ".jpg" : "https://silistrik.apiwa.tech/images/img.jpg";
 				$response = notifWa(
 					$token,
 					$reciver,
