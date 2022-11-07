2131786a9d959dc7b6ede06c30db9baf3761b368
Call Vin <kanggara75@gmail.com>
2022-11-07 21:19:49 +0700

    fix: remove sensor log

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
   {
     $this->call([
       UserSeeder::class,
-      WebhookSeeder::class,
       BotSeeder::class,
       SensorLogSeeder::class,
       DeviceLogSeeder::class,
