3e52882b1b867cbd97a3825e4f1eb25d26b22241
Call Vin <kanggara75@gmail.com>
2022-11-07 20:46:33 +0700

    add: #api cron job generate image

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -2,31 +2,71 @@
 
 namespace App\Console;
 
+use Exception;
+use App\Models\Status;
+use App\Models\SensorLog;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		// Remove unused file
+		$schedule->call(function () {
+			$unusedFile = Status::select('images')->where('isUsage', 0);
+			$toDelete = $unusedFile->get();
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+			$delete = [];
+			foreach ($toDelete as $value) {
+				$delete[] = $value->images;
+			}
+			$deleted =  Storage::delete($delete);
+			if ($deleted) {
+				$unusedFile->delete();
+			}
+		});
 
-        require base_path('routes/console.php');
-    }
+		// Generate image file
+		$schedule->call(function () {
+			$data = SensorLog::orderBy('updated_at', 'desc')->first();
+			$time = time();
+			$image = getImage($time);
+			$path = "images/" .  $time . '.' . 'jpg';
+			$filename = public_path($path);
+			$isExist = file_exists($filename);
+
+			if (!$isExist || !$image) {
+				throw new Exception("File Not Found", 1);
+			} else {
+				$status = new Status();
+				$status->api = $data->api;
+				$status->arus = $data->arus;
+				$status->asap = $data->asap;
+				$status->voltase = $data->voltase;
+				$status->temperatur = $data->temperatur;
+				$status->kelembaban = $data->kelembaban;
+				$status->images = $path;
+				$status->save();
+			}
+		});
+	}
+
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__ . '/Commands');
+		require base_path('routes/console.php');
+	}
 }
