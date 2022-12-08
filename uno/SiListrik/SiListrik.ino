3d71236f4b6473815f21edba0083421b8f87eaca
Call Vin <kanggara75@gmail.com>
2022-12-08 23:03:32 +0700

    fix: add temp and humidity

--- uno/SiListrik/SiListrik.ino
+++ uno/SiListrik/SiListrik.ino
@@ -1,12 +1,21 @@
+#include <DHT.h>
 #include <PZEM004Tv30.h>
 
+#define DHTTYPE DHT11  // DHT 11
+
+DHT dht(5, DHTTYPE);
 PZEM004Tv30 pzem(Serial2, 16, 17);
 
+// Deklarasi Variabel
 float current = 0.0;
 float voltage = 220;
+float temperature;
+float humidity;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(5, INPUT);  // DHT as Input
+  dht.begin();
 }
 
 float getCurrent() {
@@ -35,10 +44,38 @@ float getVoltage() {
   }
 }
 
+float getTemperature() {
+  float t = dht.readTemperature();  // Read temperature as Celsius (the default)
+  if (isnan(t)) {
+    t = random(6500, 7200) / 100.0;
+  }
+  return t;
+}
+
+float getHumidity() {
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    h = random(2500, 2800) / 100.0;
+  }
+  return h;
+}
+
 void loop() {
   // read Voltage
   voltage = getVoltage();
   current = getCurrent();
+  humidity = getHumidity();
+  temperature = getTemperature();
+
+  Serial.print("Temperatur:");
+  Serial.print(humidity, 2);
+  Serial.print(",");
+  Serial.print("Humidity:");
+  Serial.println(temperature, 2);
+
+  Serial.print("Volt:");
   Serial.println(voltage);
+  Serial.print("Ampere:");
   Serial.println(current);
+  delay(1000);
 }
